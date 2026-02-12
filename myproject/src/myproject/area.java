ced81084c4378523cca78deb4bd58396a2a8234e
iamprakash0304 <iamprakash0304@gmail.com>
2026-02-12 23:08:44 +0530

    area

---
+++ myproject/src/myproject/area.java
@@ -0,0 +1,15 @@
+package myproject;
+
+public class area {
+
+	public static void main(String args[]) {
+		// TODO Auto-generated method stub
+        double radi=8.9;
+        double pi=3.14;
+        double area=(radi*radi*pi);
+//we use double for all float literals 
+        System.out.println(area);
+        
+	}
+
+}
