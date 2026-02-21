6bc92d65f303a2971d790309a4aa0b1416726c76
iamprakash0304 <iamprakash0304@gmail.com>
2026-02-21 17:34:06 +0530

    Typecasting int-->Others

---
+++ iamprakash/src/iamprakash/typecast.java
@@ -0,0 +1,21 @@
+package iamprakash;
+//import java.util.Scanner;
+public class typecast {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+	//type casting
+	    int num=(int)(6.34f);
+	    System.out.println(num);
+	//automatic type casting
+	    int a=14;
+	    byte b=(byte)a;
+	    float c=(float)a;
+	    double d=(double)a;
+	    System.out.println("Type casting values are   \nint: "+a+"\nbyte: "+b+"\nfloat: "+c+"\ndouble: "+d);
+	
+	
+	}
+
+}
