1c02b31fd5d91326a41f381657918f49ae69f056
iamprakash0304 <iamprakash0304@gmail.com>
2026-02-21 17:33:08 +0530

    Scanner datatype and object

---
+++ iamprakash/src/iamprakash/scann.java
@@ -0,0 +1,15 @@
+package iamprakash;
+
+import java.util.Scanner;
+
+public class scann {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hey DAWG");
+		Scanner input=new Scanner(System.in);
+	    System.out.print("enter : ");
+		System.out.println(input.nextInt());
+	}
+
+}
